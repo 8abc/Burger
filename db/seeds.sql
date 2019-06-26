949afa716e585ef43294d84fcd8cc565c38c2feb
8abc <anadelaine.catalla@gmail.com>
2019-06-25 22:02:51 -0700

    created burgers database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Super Duper Burger", "true"),
+VALUES ("Smash Burger", "false"),
+VALUES ("In-n-Out Burger", "false");
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const express = require("express");
 const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.engine("handlebars", exphbs());
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 app.get("/", function (req, res) {
-    res.render("home");
+    res.render("index");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log("Server is starting on PORT : ", PORT);
+});

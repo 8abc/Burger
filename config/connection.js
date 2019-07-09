42a762686d70f13b044573a55f18f866eb268a55
8abc <anadelaine.catalla@gmail.com>
2019-07-09 00:09:03 -0700

    connected node to mysql

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,15 @@ const mysql = require("mysql");
 let connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "password",
     database: "burgers_db"
 });
 
-connection.connect();
+connection.connect(function(err){
+    if(err) {
+        return console.error("error: " + err.message);
+    }
+    console.log("connected as id " + connection.threadId+ "\n");
+});
 
 module.exports = connection;
